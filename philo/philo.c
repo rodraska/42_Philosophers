e01f8a6c3119beee3a40efc4a44e5495c0cbaefc
Rodrigo Reis De Carvalho <rreis-de@c2r4s2.42lisboa.com>
2023-03-22 11:17:02 +0000

    less go

--- philo/philo.c
+++ philo/philo.c
@@ -1,5 +1,79 @@
 #include "philo.h"
 
+/* void    *routine()
+{
+    printf("Start Thread\n");
+    sleep(3);
+    printf("End Thread\n");
+} */
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void    *routine()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        pthread_mutex_lock(&mutex);
+        mails++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int ft_join_threads(t_table mesa)
+{
+    int i;
+
+    i = 0;
+    while (++i < mesa.nphilo)
+    {
+        if (pthread_join(mesa.philos[i], NULL))
+        {
+            printf("Error joining thread\n");
+            return (-1);
+        }
+    }
+    return (0);
+}
+
+int ft_init_threads(t_table mesa)
+{
+    int i;
+
+    i = -1;
+    while (++i < mesa.nphilo)
+    {
+        if (pthread_create(&mesa.philos[i], NULL, &routine, NULL) != 0)
+        {
+            printf("Error creating thread\n");
+            return (-1);
+        }
+    }
+    ft_join_threads(mesa);
+    return (0);
+}
+
+void    ft_table(int ac, char **av)
+{
+    t_table mesa;
+    int     i;
+
+    i = -1;
+    mesa.nphilo = ft_atoi(av[1]);
+    mesa.philos = (pthread_t *)malloc(sizeof(pthread_t) * mesa.nphilo);
+    while (++i < mesa.nphilo)
+        mesa.philos[i] = (pthread_t)malloc(sizeof(pthread_t));
+    mesa.forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * mesa.nphilo);
+    i = -1;
+    while (++i < mesa.nphilo)
+        pthread_mutex_init(&mesa.forks[i], NULL);
+    mesa.t_die = ft_atoi(av[2]);
+    mesa.t_eat = ft_atoi(av[3]);
+    mesa.t_slp = ft_atoi(av[4]);
+    ft_init_threads(mesa);
+    printf("number of mails: %d\n", mails);
+}
+
 int parse_args(int ac, char **av)
 {
     int i;
@@ -26,7 +100,8 @@ int main(int ac, char **av)
             return (EXIT_FAILURE);
         else
         {
-            write(1, "Success\n", 8);
+            //write(1, "Success\n", 8);
+            ft_table(ac, av);
             return (EXIT_SUCCESS);
         }
     }
--- philo/philo.h
+++ philo/philo.h
@@ -16,14 +16,19 @@ typedef struct s_philo
 
 typedef struct s_table
 {
-    t_philo *philo;
-    int     total;
+    pthread_t *philos;
+    pthread_mutex_t *forks;
+    int     nphilo;
     int     t_die;
     int     t_eat;
     int     t_slp;
 }   t_table;
 
 int parse_args(int ac, char **av);
+void    ft_table(int ac, char **av);
+int ft_init_threads(t_table mesa);
+int ft_join_threads(t_table mesa);
+void    *routine();
 
 int	ft_atoi(const char *str);
 int check_digits(char *str);
